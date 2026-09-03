1751799e658c5f975ad72f0bbc06a57434b84f7c
Kurosaki077 <hayden.howell710@gmail.com>
2026-09-03 12:13:46 -0500

    Added comments and documentation

--- calculator.c
+++ calculator.c
@@ -1,27 +1,33 @@
 # include <stdio.h>
 
+//Addition function
 double add(double num1, double num2){
 	return num1 + num2;
 }
 
+//Subtraction function
 double subtract(double num1, double num2){
 	return num1 - num2;
 }
 
+//Multiplication function
 double multiply(double num1, double num2){
 	return num1 * num2;
 }
 
+//Division function
 double divide(double num1, double num2){
 	return num1 / num2;
 }
  
 int main() {
+	//Defining variables
 	int operation;
 	double num1;
 	double num2;
 	double answer; 	
 
+	//Displays calculator options
 	printf("Calculator\n");
 	printf("1. Add\n");
 	printf("2. Subtract\n");
@@ -30,11 +36,13 @@ int main() {
 
 	printf("Choose an operation: ");
 	
+	//Scans operation and checks validity
 	if(scanf("%d", &operation) != 1){
 		printf("Invalid input\n");
 		return 1;
 	}
 
+	//Checks if operation is within the given range
 	if(operation < 1 || operation > 4){
 		printf("Invalid operation\n");
 		return 1;
@@ -42,6 +50,7 @@ int main() {
 
 	printf("Enter first number: ");
 	
+	//Scans first number and checks validity
 	if(scanf("%lf", &num1) != 1){
 		printf("Invalid number\n");
 		return 1;
@@ -49,11 +58,13 @@ int main() {
 
 	printf("Enter second number: ");
 	
+	//Scans second number and checks validity
 	if(scanf("%lf", &num2) != 1){
 		printf("Invalid number\n");
 		return 1;
 	}
 
+	//Executes the given operation
 	switch(operation){
 		case 1:
 			answer = add(num1, num2);
@@ -71,6 +82,7 @@ int main() {
 			break;
 
 		case 4:
+			//Checks division by 0
 			if(num2 == 0){
 				printf("Can't divide by zero\n");
 			}
