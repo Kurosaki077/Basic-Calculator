bcb1794020bac82acbca95640b880163932f803c
Kurosaki077 <hayden.howell710@gmail.com>
2026-09-03 10:53:10 -0500

    Implemented addition feature

--- calculator.c
+++ calculator.c
@@ -1,5 +1,12 @@
 # include <stdio.h>
 
+double add(double num1, double num2){
+	return num1 + num2;
+}
+
 int main() {
- 
+ 	double answer;
+	answer = add(7.7, 5.4);
+	printf("Addition: %.2f\n", answer);
+	return 0;
  }
