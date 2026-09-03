c47455c4ffd0630a93a74b9d68a53a9d5af62f1f
Kurosaki077 <hayden.howell710@gmail.com>
2026-09-03 11:00:09 -0500

    Implemented subtraction feature

--- calculator.c
+++ calculator.c
@@ -4,9 +4,18 @@ double add(double num1, double num2){
 	return num1 + num2;
 }
 
+double subtract(double num1, double num2){
+	return num1 - num2;
+}
+
 int main() {
  	double answer;
+
 	answer = add(7.7, 5.4);
 	printf("Addition: %.2f\n", answer);
+
+	answer = subtract(4.5, 2.7);
+	printf("Subtraction: %.2f\n", answer);
+
 	return 0;
  }
